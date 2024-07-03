43979ab578861c8441b45e4973765529905f2ea8
David Márquez Fàbrega <32216320+DavidMarquezF@users.noreply.github.com>
2024-07-03 07:14:13 +0200

    Update plugin.php

--- recaptchav2/plugin.php
+++ recaptchav2/plugin.php
@@ -1,7 +1,7 @@
 <?php
 return array(
     'id' =>             'recaptcha',
-    'version' =>        '0.3',
+    'version' =>        '0.4',
     'name' =>           'Recaptcha',
     'author' =>         'David Márquez',
     'description' =>    'Provides google recaptcha v2 as a form field type to avoid spam',
